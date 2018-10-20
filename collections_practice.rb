f98fed87593424c39f2237f0fc52f190d3ac5f65
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-20 15:05:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,8 +35,7 @@ def merged_data(keys,data)
   #keys.map{|e| data e.keys #e.values}
   #data.map{|e| e.values}
   #keys.map{|e,i| data[e[:first_name]]}
-  data.each{|e|
-    puts e }
+  data.each{|e| }
 
 end
 puts merged_data(
