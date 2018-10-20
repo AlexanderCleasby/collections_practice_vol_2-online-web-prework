e90194841ad4b5d3fe3891d7f424537ef57355c9
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-20 14:06:57 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
 # your code goes here
+require "pry"
+
+def begins_with_r(array)
+  array.all? { |e| e[0]=="r" }
+end
+
+def contain_a(array)
+  array.delete_if{ |e| !e.include?('a')}
+end
+
+def first_wa(array)
+  out = ""
+  array.each{|e|
+    if e[0,2]=="wa"
+      out=e
+      break
+    end}
+    out
+end
+
+def remove_non_strings(array)
+  array.delete_if{ |e|
+    e.class != String
+  }
+end
+
+def count_elements(array)
+  array = array.map{|e| e[:name]}
+  array.uniq.map{|e|
+  {:name => e,:count => array.count(e)} }
+end
+
+def merged_data(keys,data)
+  keys.keys
+end
+#puts count_elements([{:name => "blake"}, {:name => "blake"}, {:name => "ashley"}])
+#binding.pry
